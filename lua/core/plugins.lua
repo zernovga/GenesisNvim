c1f4787b6fae439559b8dd71aaeab44fabef0164
zernovga <zernovga@gmail.com>
2024-06-12 00:25:57 +0300

    Feat: added VenvSelect plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,182 +1,138 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-	
-	{ 'nvim-treesitter/nvim-treesitter' },
-	{ 'neovim/nvim-lspconfig' },
-
-	-- Autocomplete support
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-buffer' },
-	{ 'hrsh7th/cmp-path' },
-	{ 'hrsh7th/cmp-cmdline' },
-	{ 'hrsh7th/nvim-cmp' },
-	{ 'williamboman/mason.nvim' },
-
-    {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-
-	{
-	  'nvimdev/dashboard-nvim',
-	  event = 'VimEnter',
-	  config = function()
-	    require('dashboard').setup {
-	      -- config
-	    }
-	  end,
-	  dependencies = { {'nvim-tree/nvim-web-devicons'}}
-	},
-
-	{ 'Eandrju/cellular-automaton.nvim' },
-	{ 'norcalli/nvim-colorizer.lua' },
-
-	{
-	    'nvim-lualine/lualine.nvim',
-	    dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
-
-	{
-	  "folke/todo-comments.nvim",
-	  dependencies = { "nvim-lua/plenary.nvim" },
-	  opts = {
-	    -- your configuration comes here
-	    -- or leave it empty to use the default settings
-	    -- refer to the configuration section below
-	  }
-	},
-
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-	{
-	  "max397574/better-escape.nvim",
-	  config = function()
-  	    require("better_escape").setup({
-  	    	mapping = {"jk"},
-  	    	timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-    	    clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-    	    keys = "<Esc>",
-  	    })
-  	  end
-	},
-
-	{
-	    'numToStr/Comment.nvim',
-	    opts = {
-	        -- add any options here
-	    },
-	    lazy = false,
-	},
-
-	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
-
-	{
-	  "nvim-tree/nvim-tree.lua",
-	  version = "*",
-	  lazy = false,
-	  dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	  },
-	  config = function()
-	    require("nvim-tree").setup {}
-	  end,
-	},
-
-	{
-	    'dense-analysis/ale',
-	    config = function()
-	        -- Configuration goes here.
-	        local g = vim.g
-	
-	        g.ale_linters = {
-	        	python = {'mypy'},
-	            lua = {'lua_language_server'}
-	        }
-	    end
-	},
-
-	{ 'RRethy/vim-illuminate' },
-
-	{
-	    "vhyrro/luarocks.nvim",
-	    priority = 1001, -- this plugin needs to run before anything else
-	    opts = {
-	        rocks = { "magick" },
-	    },
-	},
-
-	{
-	 "folke/trouble.nvim",
-	 dependencies = { "nvim-tree/nvim-web-devicons" },
-	 opts = {
-	  -- your configuration comes here
-	  -- or leave it empty to use the default settings
-	  -- refer to the configuration section below
-	 },
-	},
-
-	{'akinsho/toggleterm.nvim', version = "*", config = true},
-
-	{
-	  "folke/which-key.nvim",
-	  event = "VeryLazy",
-	  init = function()
-	    vim.o.timeout = true
-	    vim.o.timeoutlen = 300
-	  end,
-	  opts = {
-	    -- your configuration comes here
-	    -- or leave it empty to use the default settings
-	    -- refer to the configuration section below
-	  }
-	},
-
-	-- Выравнивание и перемещение текста
-	-- Автоматическое открытие фигурных скобок, кавычек и т.д
-	{ 'echasnovski/mini.nvim', version = false },
-	{ 'echasnovski/mini.move', version = false },
-	{ 'echasnovski/mini.pairs', version = false },
-
-    {'linux-cultist/venv-selector.nvim',
-      dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-      opts = {
-          -- Your options go here
-          name = ".venv",
-          -- auto_refresh = false
-      },
-      event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymappingkeys
-      keys = {
-          -- Keymap to open VenvSelector to pick a venv.
-          { '<leader>vs', '<cmd>VenvSelect<cr>' },
-          -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-          { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-      },
+require("lazy").setup({{'nvim-treesitter/nvim-treesitter'}, {'neovim/nvim-lspconfig'}, -- Autocomplete support
+{'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'}, {'hrsh7th/cmp-cmdline'}, {'hrsh7th/nvim-cmp'},
+                       {'williamboman/mason.nvim'}, {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.6',
+    dependencies = {'nvim-lua/plenary.nvim'}
+}, {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+        require('dashboard').setup {
+            -- config
+        }
+    end,
+    dependencies = {{'nvim-tree/nvim-web-devicons'}}
+}, {'Eandrju/cellular-automaton.nvim'}, {'norcalli/nvim-colorizer.lua'}, {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {'nvim-tree/nvim-web-devicons'}
+}, {
+    "folke/todo-comments.nvim",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}, {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = ...
+}, {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+}, {
+    "max397574/better-escape.nvim",
+    config = function()
+        require("better_escape").setup({
+            mapping = {"jk"},
+            timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+            clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+            keys = "<Esc>"
+        })
+    end
+}, {
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
     },
-    {'AckslD/swenv.nvim',
-  -- Should return a list of tables with a `name` and a `path` entry each.
-  -- Gets the argument `venvs_path` set below.
-  -- By default just lists the entries in `venvs_path`.
-  get_venvs = function(venvs_path)
-    return require('swenv.api').get_venvs(venvs_path)
-  end,
-  -- Path passed to `get_venvs`.
-  venvs_path = vim.fn.expand('~/venvs'),
-  -- Something to do after setting an environment, for example call vim.cmd.LspRestart
-  post_set_venv = nil,
-}
+    lazy = false
+}, {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons'
+}, {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function()
+        require("nvim-tree").setup {}
+    end
+}, {
+    'dense-analysis/ale',
+    config = function()
+        -- Configuration goes here.
+        local g = vim.g
 
-})
+        g.ale_linters = {
+            python = {'mypy'},
+            lua = {'lua_language_server'}
+        }
+    end
+}, {'RRethy/vim-illuminate'}, {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+        rocks = {"magick"}
+    }
+}, {
+    "folke/trouble.nvim",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}, {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true
+}, {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    }
+}, -- Выравнивание и перемещение текста
+-- Автоматическое открытие фигурных скобок, кавычек и т.д
+{
+    'echasnovski/mini.nvim',
+    version = false
+}, {
+    'echasnovski/mini.move',
+    version = false
+}, {
+    'echasnovski/mini.pairs',
+    version = false
+}, {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {"neovim/nvim-lspconfig", "mfussenegger/nvim-dap", "mfussenegger/nvim-dap-python", -- optional
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {"nvim-lua/plenary.nvim"}
+    }},
+    lazy = false,
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+        require("venv-selector").setup()
+    end,
+    keys = {{",v", "<cmd>VenvSelect<cr>"}}
+}})
